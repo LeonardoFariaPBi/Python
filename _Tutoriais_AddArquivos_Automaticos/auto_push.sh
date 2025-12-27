661d43350997c90a7dff2889ab93f6d4690935c4
LeonardoFariaPBi <leonardofaria1995@gmail.com>
2025-12-27 19:19:24 -0300

    Update automático

---
+++ _Tutoriais_AddArquivos_Automaticos/auto_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "📌 Repositório: Hashtag/Python"
+
+if git diff --quiet && git diff --cached --quiet; then
+    echo "✅ Nenhuma alteração para enviar."
+    exit 0
+fi
+
+git add .
+git commit -a -m "Update automático"
+git push
+
+echo "🚀 Push finalizado!"
